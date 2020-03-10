60beb19085e8e396f1f104442009de0bee40e765
sujithvn <mail.sujithvn@gmail.com>
2020-03-10 11:12:20 +0530

    Views & Routes for List detail, Search, Dashboard

--- controllers/list.js
+++ controllers/list.js
@@ -3,3 +3,11 @@ const path = require("path");
 exports.getListingAll = (req, res, next) => {
   res.render(path.join(__dirname, "..", "views", "listing_all"));
 };
+
+exports.getListingDetail = (req, res, next) => {
+  res.render(path.join(__dirname, "..", "views", "listing_detail"));
+};
+
+exports.postSearch = (req, res, next) => {
+  res.render(path.join(__dirname, "..", "views", "search"));
+};
--- routes/list.js
+++ routes/list.js
@@ -5,5 +5,7 @@ const listController = require("../controllers/list");
 const router = express.Router();
 
 router.get("/listing_all", listController.getListingAll);
+router.get("/listing_detail", listController.getListingDetail);
+router.post("/search", listController.postSearch);
 
 module.exports = router;
